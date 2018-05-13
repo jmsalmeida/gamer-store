a9a5b090be629628e6b8bd15e45370089531f0a1
James Almeida <james.almeida.ti@gmail.com>
2018-05-13 06:54:39 -0300

    Adiciona Banner

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Mapa from './components/Mapa/Mapa';
 import Header from './components/Header/Header';
+import Banner from './components/Banner/Banner';
 
 class App extends Component {
   componentWillMount(){
@@ -13,6 +14,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
+        <Banner/>
         {/* <Mapa mapa={this.mapa}/> */}
       </div>
     );
